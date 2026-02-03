61ac9d52dd4a03012d4b866388f430c2ea23449f
19deagostoutn <19deagosto.utn@gmail.com>
2026-02-03 13:16:50 -0300

    Update copyright year in footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -50,7 +50,7 @@ export function Footer() {
           </div>
         </div>
         <div className="border-t border-secondary-700 mt-8 pt-8 text-center text-secondary-400">
-          <p>&copy; 2025 Agrupación 19 de Agosto - UTN FRBA. Todos los derechos reservados.</p>
+          <p>&copy; 2026 Agrupación 19 de Agosto - UTN FRBA. Todos los derechos reservados.</p>
         </div>
       </div>
     </footer>
